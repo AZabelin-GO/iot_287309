e040ad4a8f4abea63a34af808e707608e3aa82f7
Aleksey Zabelin <zabelin.av@dclouds.ru>
2023-01-27 18:42:07 +0700

    ISSUE-XXX: add tasks for lesson 6

---
+++ lesson-6/task-41/Program.cs
@@ -0,0 +1,34 @@
+﻿// Task 41
+// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+Console.WriteLine("----- Task 41 -----");
+Console.WriteLine("Enter Numbers; enter empty line to stop");
+
+void main() {
+    int numbersCounter = 0;
+
+    while (true)
+    {
+        string num = Console.ReadLine().Trim();
+
+        if (num.Length == 0) {
+            break;
+        }
+
+        int tmp;
+        if (!int.TryParse(num, out tmp)) {
+            Console.WriteLine("Error: cannot parse number");
+            Environment.Exit(1);
+        }
+
+        if (tmp > 0) {
+            numbersCounter++;
+        }
+    }
+
+    Console.WriteLine($"Amount of numbers greater than 0 is {numbersCounter}");
+}
+
+main();
+
+Console.WriteLine("------------------");
---
+++ lesson-6/task-43/Program.cs
@@ -0,0 +1,50 @@
+﻿// Task 43
+// Напишите программу, которая найдёт точку пересечения двух прямых,
+// заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+
+Console.WriteLine("----- Task 43 -----");
+Console.WriteLine("Enter FOUR numbers: k1, b1, k2, b2");
+
+int[] readNumbers(int n)
+{
+    int[] res = new int[n];
+
+    for (int i = 0; i < n; i++)
+    {
+        string num = Console.ReadLine().Trim();
+
+        int tmp;
+        if (!int.TryParse(num, out tmp))
+        {
+            Console.WriteLine("Error: cannot parse number");
+            Environment.Exit(1);
+        }
+
+        res[i] = tmp;
+    }
+
+    return res;
+}
+
+(float x, float y) getLinesIntersection(int[] consts)
+{
+    float x = (float) (consts[3] - consts[1]) / (consts[0] - consts[2]);
+    float y = consts[0] * x + consts[1];
+
+    return (x, y);
+}
+
+void main()
+{
+    int[] consts = readNumbers(4);
+
+    Console.WriteLine($"{consts}");
+
+    var (x, y) = getLinesIntersection(consts);
+
+    Console.WriteLine($"({x}, {y})");
+}
+
+main();
+
+Console.WriteLine("------------------");
