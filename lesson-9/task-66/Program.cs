36d0813182acfc1fa77ba4f86e95971606769d0a
Aleksey Zabelin <zabelin.av@dclouds.ru>
2023-02-05 14:59:24 +0700

    ISSUE-XXX: add tasks for lesson 9

---
+++ lesson-9/task-66/Program.cs
@@ -0,0 +1,32 @@
+﻿// Task 66
+// Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+Console.WriteLine("----- Task 66 -----");
+
+(int m, int n) genTwoNumbers(){
+    Random rnd = new Random();
+
+    int m = rnd.Next(0, 10);
+    int n = rnd.Next(0, 10);
+
+    if (m > n ) {
+        return (n, m);
+    }
+
+    return (m, n);
+}
+
+int calcSumBetweenNumbers(int m, int n) {
+    if (m == n) {
+        return m;
+    }
+
+    return n + calcSumBetweenNumbers(m, n - 1);
+}
+
+void main() {
+    var (m, n) = genTwoNumbers();
+    Console.WriteLine($"The sum of numbers between {m} and {n} is {calcSumBetweenNumbers(m, n)}");
+}
+
+main();
---
+++ lesson-9/task-68/Program.cs
@@ -0,0 +1,30 @@
+﻿// Task 68
+// Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+
+Console.WriteLine("----- Task 68 -----");
+
+(int m, int n) genTwoNumbers(){
+    Random rnd = new Random();
+
+    int m = rnd.Next(0, 5);
+    int n = rnd.Next(0, 5);
+
+    return (m, n);
+}
+
+int ack(int m, int n) {
+    if (m == 0) {
+        return n + 1;
+    } else if (n == 0) {
+        return ack(m - 1, 1);
+    }
+
+    return ack(m - 1, ack(m, n - 1));
+}
+
+void main() {
+    var (m, n) = genTwoNumbers();
+    Console.WriteLine($"Ack({m}, {n}) = {ack(m, n)}");
+}
+
+main();
